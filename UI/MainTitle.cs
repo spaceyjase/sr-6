a8b462b0a57d4b2fca8a2a9a0d3e7a3515630b34
Jason <429978+spaceyjase@users.noreply.github.com>
2021-11-27 17:45:39 +0000

    Another section.

--- UI/MainTitle.cs
+++ UI/MainTitle.cs
@@ -5,6 +5,12 @@ public class MainTitle : Control
 {
   [Export] private string worldScenePath;
 
+  public override void _Ready()
+  {
+    base._Ready();
+    GetNode<Button>("PlayButton").GrabFocus();
+  }
+
   private void OnPlayButton_Pressed()
   {
     GetTree().ChangeScene(worldScenePath);
