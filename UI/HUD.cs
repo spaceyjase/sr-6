2a8be403f6e7dccba3d2492728813783beb066e7
Jason <429978+spaceyjase@users.noreply.github.com>
2021-11-29 12:57:14 +0000

    Show game time for game over (win!).

--- UI/HUD.cs
+++ UI/HUD.cs
@@ -1,26 +1,46 @@
+using System;
 using Godot;
 using Godot.Collections;
 
 namespace UI
 {
-  public class HUD : MarginContainer
+  public class HUD : Control
   {
     private Array<TextureRect> lifeCounter;
     private Label batteryLabel;
+    private Label gameTimeLabel;
     private int batteryCount = -1; // HACK for initial score update.
     private AnimationPlayer animationPlayer;
+
+    private double timer;
 
     public override void _Ready()
     {
       lifeCounter = new Array<TextureRect>
       {
-        GetNode<TextureRect>("HBoxContainer/LifeCounter/L1"),
-        GetNode<TextureRect>("HBoxContainer/LifeCounter/L2"),
-        GetNode<TextureRect>("HBoxContainer/LifeCounter/L3")
+        GetNode<TextureRect>("PlayerDetails/HBoxContainer/LifeCounter/L1"),
+        GetNode<TextureRect>("PlayerDetails/HBoxContainer/LifeCounter/L2"),
+        GetNode<TextureRect>("PlayerDetails/HBoxContainer/LifeCounter/L3")
       };
-      batteryLabel = GetNode<Label>("HBoxContainer/HBoxContainer/BatteryLabel");
-      animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+      gameTimeLabel = GetNode<Label>("GameTime/GameTimeLabel");
+      batteryLabel = GetNode<Label>("PlayerDetails/HBoxContainer/HBoxContainer/BatteryLabel");
+      animationPlayer = GetNode<AnimationPlayer>("PlayerDetails/AnimationPlayer");
     }
+
+    public override void _Process(float delta)
+    {
+      base._Process(delta);
+      
+      timer += delta;
+      
+      // format output in minutes and hours from seconds
+      var minutes = Math.Floor(timer / 60);
+      var hours = (int)Math.Floor(minutes / 60);
+      minutes %= 60;
+      gameTimeLabel.Text = $"{hours:00}:{minutes:00}:{timer % 60:00.00}";
+    }
+
+    public double GameTime => timer;
 
     private async void OnPlayer_Life_Changed(int value)
     {
